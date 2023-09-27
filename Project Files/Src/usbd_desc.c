66bffcb2427e7ac1b0c74bc9239ce88d90f4c9fb
Максим Чуприн <mchuprin@eltaltd.ru>
2023-09-27 17:07:34 +0300

    no message

--- Project Files/Src/usbd_desc.c
+++ Project Files/Src/usbd_desc.c
@@ -61,8 +61,8 @@ const uint8_t hUSBDDeviceDesc[USB_LEN_DEV_DESC]= {
   USB_DESC_TYPE_DEVICE,       /* bDescriptorType */
   0x00,                       /* bcdUSB */
   0x02,
-  0x00,                       /* bDeviceClass */
-  0x00,                       /* bDeviceSubClass */
+  0x02,                       /* bDeviceClass */
+  0x02,                       /* bDeviceSubClass */
   0x00,                       /* bDeviceProtocol */
   USB_MAX_EP0_SIZE,           /* bMaxPacketSize */
   LOBYTE(USBD_VID),           /* idVendor */
